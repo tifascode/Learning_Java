5354634484f8590be68cbe67392baf39f40ad8e3
Kaua Fraga <fragakaua@gmail.com>
2025-05-22 19:43:57 -0300

    Atributos em Interfaces

--- Interfaces/src/Criando_Interfaces/Walkable.java
+++ Interfaces/src/Criando_Interfaces/Walkable.java
@@ -2,6 +2,8 @@ package Criando_Interfaces;
 
 public interface Walkable {
 
+    // Atributos em Interfaces são public, static e final
+
     void walk(); // Métodos em interfaces são abstracts e public obrigatoriamente
     void stop();
 }
