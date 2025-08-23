12655434c8aa0effa1a5fbe48e1fde24cbfe43b4
Kaua Fraga <fragakaua@gmail.com>
2025-08-23 09:42:37 -0300

    Aprendendo sobre Metacaracteres e Quantificadores

---
+++ Expressoes_regulares/src/Metacaracteres/App.java
@@ -0,0 +1,21 @@
+package Metacaracteres;
+
+public class App {
+    public static void main(String[] args) {
+
+        // META CARACTERES
+        // -> /d (digitos 0-9)
+        // -> /s (espaço em branco)
+        // -> /w (caracteres alfa-numericos: digitos, letras, "_")
+        // -> .  (qualquer caractere)
+        // -> [abc] (ou "a" ou "b" ou "c")
+        // -> [a-f] (qualquer caractere de "a" até "f")
+        // -> [a-f0-9] (qualquer caractere de "a" até "f" ou de "0" até "9")
+
+        // QUANTIFICADORES
+        // -> * (zero ou mais ocorrências)
+        // -> + (Uma ou mais ocorrências)
+        // -> ? (nenhuma ou uma ocorrência)
+
+    }
+}
