6c15aaed2bcd21742c68a8934fd94d127fcecdaa
Kaua Fraga <fragakaua@gmail.com>
2025-08-26 19:53:08 -0300

    BigDecimal e falhas de arredondamentos

---
+++ BigNumbers/src/BigDecimal/App.java
@@ -0,0 +1,21 @@
+package BigDecimal;
+
+import java.math.BigDecimal;
+
+public class App {
+    public static void main(String[] args) {
+
+        BigDecimal big = new BigDecimal("10000000");
+        big = big.add(BigDecimal.ONE);
+        big = big.multiply(BigDecimal.TEN);
+        System.out.println(big.toString());
+
+        double d1 = 0.1;
+        double d2 = 0.2;
+        System.out.println(d1+d2); // saída -> 0.30000000000000004
+
+        BigDecimal b1 = new BigDecimal("0.1");
+        BigDecimal b2 = new BigDecimal("0.2");
+        System.out.println(b1.add(b2)); // saída -> 0.3
+    }
+}
