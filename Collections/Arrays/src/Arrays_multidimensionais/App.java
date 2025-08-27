5efa256d30e09c224622de8581dc05728ddb59b7
Kaua Fraga <fragakaua@gmail.com>
2025-08-27 20:20:29 -0300

    Acessando posições de  arrays multidimensionais

---
+++ Collections/Arrays/src/Arrays_multidimensionais/App.java
@@ -0,0 +1,17 @@
+package Arrays_multidimensionais;
+
+public class App {
+    public static void main(String[] args) {
+
+        int[][] array = new int[5][4];
+        int[][][] array2 = new int[5][4][5];
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j]);
+            }
+            System.out.println();
+        }
+
+    }
+}
