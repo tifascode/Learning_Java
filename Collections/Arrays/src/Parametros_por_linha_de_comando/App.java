80cbc43f547f8fcc2523bb254869f2234a17d040
Kaua Fraga <fragakaua@gmail.com>
2025-08-29 20:36:30 -0300

    Fornecendo parâmetros por linha de comando

---
+++ Collections/Arrays/src/Parametros_por_linha_de_comando/App.java
@@ -0,0 +1,10 @@
+package Parametros_por_linha_de_comando;
+
+import java.util.Arrays;
+
+public class App {
+    public static void main(String[] args) {
+
+        System.out.println(Arrays.toString(args));
+    }
+}
